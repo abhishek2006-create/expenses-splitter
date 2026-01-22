5ee644941828160541559c8b0ed0d39cd5500bde
abhishek2006-create <abhishek.2006.bairwa@gmail.com>
2026-01-22 22:55:28 +0530

    Update Navbar.jsx

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -3,15 +3,12 @@ import "../styling/nav.css"
 import splitImg from "../assets/split.png"
 
 export default function Navbar(){
-  
     return(
         <>
         <div className="Navbar">
             <img src={splitImg} alt="logo" />
             <h1>Expense Splitter</h1>
-            <div>
-            </div>
         </div>
         </>
-    );
+    );  
 }
