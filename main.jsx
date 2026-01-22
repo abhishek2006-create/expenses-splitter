c1fe822f9de911c73cbe54f3db5d0e50e2937384
abhishek2006-create <abhishek.2006.bairwa@gmail.com>
2026-01-22 23:00:46 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ReactDOM from "react-dom/client"
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+     <App />
+    </HashRouter>
   </StrictMode>,
 )
